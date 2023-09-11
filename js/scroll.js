2223fe3450939a7c80965b242b6ac7f0f5ac4f01
JazzBlaze <thejasdub@gmail.com>
2023-09-11 19:13:00 +0400

    fixed

--- js/scroll.js
+++ js/scroll.js
@@ -2,17 +2,15 @@ import Lenis from '@studio-freight/lenis'
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger);
-// Import the necessary function for preloading images
+
 import { preloadImages } from './utils.js';
 
-// Define a variable that will store the Lenis smooth scrolling object
 let lenis;
 
-// Element with class .columns
 const grid = document.querySelector('.columns');
-// All the columns class .column
+
 const columns = [...grid.querySelectorAll('.column')];
-// Map each column to its array of items and keep a reference of the image, its wrapper and the column
+
 const items = columns.map((column, pos) => {
 	return [...column.querySelectorAll('.column__item')].map(item => ({
 		element: item,
@@ -24,23 +22,22 @@ const items = columns.map((column, pos) => {
 // All itemms
 const mergedItems = items.flat();
 
-// Function to initialize Lenis for smooth scrolling
 const initSmoothScrolling = () => {
-	// Instantiate the Lenis object with specified properties
+
 	lenis = new Lenis({
-		lerp: 0.15, // Lower values create a smoother scroll effect
-		smoothWheel: true // Enables smooth scrolling for mouse wheel events
+		lerp: 0.15,
+		smoothWheel: true 
 	});
 
-	// Update ScrollTrigger each time the user scrolls
+
 	lenis.on('scroll', () => ScrollTrigger.update());
 
-	// Define a function to run at each animation frame
+
 	const scrollFn = (time) => {
-		lenis.raf(time); // Run Lenis' requestAnimationFrame method
-		requestAnimationFrame(scrollFn); // Recursively call scrollFn on each frame
+		lenis.raf(time);
+		requestAnimationFrame(scrollFn); 
 	};
-	// Start the animation frame loop
+
 	requestAnimationFrame(scrollFn);
 };
 
@@ -74,7 +71,6 @@ const scroll = () => {
   	});
 }
 
-// Preload images, initialize smooth scrolling, apply scroll-triggered animations, and remove loading class from body
 preloadImages('.column__item-img').then(() => {
 	initSmoothScrolling();
 	scroll();
